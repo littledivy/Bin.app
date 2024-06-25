09a260cd54028efa7e852a589763f20745f85206
Divy Srivastava <dj.srivastava23@gmail.com>
2024-06-25 08:35:47 +0530

    Initial Commit

---
+++ notes/notesApp.swift
@@ -0,0 +1,17 @@
+//
+//  notesApp.swift
+//  notes
+//
+//  Created by Divy Srivastava on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct notesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
