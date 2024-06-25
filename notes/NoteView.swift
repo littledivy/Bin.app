c1538947fa691cfa03c41b5dce6eb5f415e29c40
Divy Srivastava <dj.srivastava23@gmail.com>
2024-06-25 22:24:45 +0530

    Add Notes view

---
+++ notes/NoteView.swift
@@ -0,0 +1,18 @@
+//
+//  NoteView.swift
+//  notes
+//
+//  Created by Divy Srivastava on 25/06/24.
+//
+
+import SwiftUI
+
+struct NoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoteView()
+}
